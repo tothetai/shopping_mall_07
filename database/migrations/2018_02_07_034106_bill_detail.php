5e1da3bb7d85a861b66d8e0482c204c3fa6d24d9
Tai To <tothetai@gmail.com>
2018-02-07 11:13:42 +0700

    Migration

---
+++ database/migrations/2018_02_07_034106_bill_detail.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BillDetail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_detail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bill_id')->unsigned();
+            $table->integer('pro_id')->unsigned();
+            $table->float('price');
+            $table->integer('quantity');
+            $table->timestamps();     
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_detail');
+    }
+}
