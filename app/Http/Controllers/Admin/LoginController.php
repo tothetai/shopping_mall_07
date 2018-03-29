0ed4b0cffe3fa3cd0bd0f935c1e223381c5d8367
hoaibao <nguyenthihoaibn96@gmail.com>
2018-03-29 14:46:36 +0700

    tesst

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -17,32 +17,21 @@ class LoginController extends Controller
         return view('backend.login');
     }
     public function postLogin(Request $request) {
-        return $request->all();
-        $rules = [
-            'email' =>'required|email',
-            'password' => 'required|min:6'
+
+        $logins = [
+            'email' => $request->email,
+            'password' => $request->password
         ];
-        $messages = [
-            'email.required' => 'Email là trường bắt buộc',
-            'email.email' => 'Email không đúng định dạng',
-            'password.required' => 'Mật khẩu là trường bắt buộc',
-            'password.min' => 'Mật khẩu phải chứa ít nhất 8 ký tự',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
 
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        } else {
-            $email = $request->input('email');
-            $password = $request->input('password');
+        if(Auth::attempt($logins)){
 
+            return redirect('backend.index');
 
-            if( Auth::attempt(['email' => $email, 'password' => $password, 'role' => [1] ])) {
-                return redirect()->intended('admin/homes');
-            } else {
-                $errors = new MessageBag(['errorlogin' => 'Email hoặc mật khẩu không đúng']);
-                return redirect()->back()->withInput()->withErrors($errors);
-            }
         }
+        else
+        {
+            return redirect('login')->with('message',' Email hoặc password sai !');
+        }
+        
     }
 }
